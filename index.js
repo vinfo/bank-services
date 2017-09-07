35ce0ac515ad5eefb1da1559f9e43b50698d9913
vinfo <victor.valencia@gmail.com>
2017-09-07 09:26:37 -0500

    feat:setOffert

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  socket.on('setOffert', function(msg){
+    io.emit('setOffert', msg);
+  });  
 });
 
 http.listen(port, function(){
