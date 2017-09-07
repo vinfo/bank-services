38baee8a9fb1b01930c3aab253843eca08242e02
vinfo <victor.valencia@gmail.com>
2017-09-07 10:11:23 -0500

    test auto

--- index.js
+++ index.js
@@ -8,21 +8,9 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('setOffert', function(msg){
+    io.emit('setOffert', msg);
   });
-  //Notificar nueva oferta
-  socket.on('setOffert', function(msg){
-    io.emit('setOffert', msg);
-  }); 
-  //Notificar nueva ofesadfasfrta
-  socket.on('setOffert', function(msg){
-    io.emit('setOffert', msg);
-  }); 
-      //Notificar nueva oferasfasfasta
-  socket.on('setOffert', function(msg){
-    io.emit('setOffert', msg);
-  }); 
 });
 
 http.listen(port, function(){
